dce30bd3525c2e430359f358b47c0b12a2927402
Victor H. T. Filho <victorhugo.tozzo@gmail.com>
2026-01-05 14:09:07 -0300

    ajustando para a api oficial

--- services/api.ts
+++ services/api.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://e698481f880d.ngrok-free.app';
+export const BASE_URL = 'https://api.tozzo.uk';
 
 async function handleJsonResponse(res: Response) {
   const txt = await res.text();
